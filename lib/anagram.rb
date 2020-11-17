a92402dbe07445c4114acb1e68bb1af2e1f0c4ce
Wesley <wes.beck21@outlook.com>
2020-11-17 13:17:15 -0800

    commit tree d5270dc6b7cf1fb44ae52dc3b037e2590bf65f41

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+class Anagram
+    
+    def initialize(anagram)     #initialize instance variable
+        @anagram = anagram
+    end
+
+#create a method to see if a set of words has an anagram within.
+    def match(words)        
+        is_anagram?(words)
+    end
+
+    #create a method to iterate over the set of words to see if the 
+# anagram word is equal to and words with the same amount of 
+#chracters sorted to be identical.
+
+    def is_anagram?(words) 
+        words.select do |word|
+            word.chars.sort == @anagram.chars.sort
+        end
+    end
+end
